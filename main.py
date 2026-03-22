c26d4c8c48a0991a337619b777f730c79a0f9600
Xiako <ivanvikulov8@gmail.com>
2026-03-23 00:42:29 +0300

    Ну наконец хуйня заработала

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ import os
 import asyncio
 import logging
 
-
 class NSTA(commands.Bot):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         @self.tree.command(name='kill')
-        async def kill():
+        async def kill(smth):
             exit()
 
         @self.event
         async def on_ready():
+            print("Bot is ready")
             await self.tree.sync()
 
         @self.event
@@ -30,9 +30,7 @@ class NSTA(commands.Bot):
         async def on_message(msg):
             pass
 
-
 bot = NSTA(command_prefix='!', intents=discord.Intents.all())
-
 token = os.getenv('DISCORD_TOKEN')
 if not token:
     raise ValueError('DISCORD_TOKEN not set in environment variables')
