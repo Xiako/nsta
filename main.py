89e328afe26960d5bb23346f52af6b3efffc3209
Xiako <ivanvikulov8@gmail.com>
2026-03-22 23:19:58 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from threading import Thread
+import discord
+from discord.ext import commands
+import json
+import requests
+import random
+import os
+import asyncio
+import logging
+
+
+class NSTA(commands.Bot):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        @self.tree.command(name='kill')
+        async def kill():
+            exit()
+
+        @self.event
+        async def on_ready():
+            await self.tree.sync()
+
+        @self.event
+        async def on_voice_state_update(member, before, after):
+            pass
+
+        @self.event
+        async def on_message(msg):
+            pass
+
+
+bot = NSTA(command_prefix='!', intents=discord.Intents.all())
+
+token = os.getenv('DISCORD_TOKEN')
+if not token:
+    raise ValueError('DISCORD_TOKEN not set in environment variables')
+
+bot.run(token, log_level=logging.WARNING)
